38781e1fe5e4398d6c1924e399d5d284e92f12c9
Kuba <jakubpiotrowicz9@gmail.com>
2023-12-30 13:30:28 +0100

    routy bez głównej logiki

--- backend/db_models.py
+++ backend/db_models.py
@@ -6,6 +6,20 @@ Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
+association_item_experiment_table = Table(
+    "association_item_experiment_table",
+    Base.metadata,
+    Column("left_id", ForeignKey("experiments.id")),
+    Column("right_id", ForeignKey("items.id")),
+)
+
+association_category_experiment_table = Table(
+    "association_category_experiment_table",
+    Base.metadata,
+    Column("left_id", ForeignKey("experiments.id")),
+    Column("right_id", ForeignKey("categories.id")),
+)
+
 class Categories(Base):
     __tablename__ = 'categories'
 
@@ -14,13 +28,6 @@ class Categories(Base):
 
     def __repr__(self):
         return f"Categories<id:{self.id}, name:{self.name}>"
-
-association_table = Table(
-    "association_table",
-    Base.metadata,
-    Column("left_id", ForeignKey("experiments.id")),
-    Column("right_id", ForeignKey("items.id")),
-)
 
 class Items(Base):
     __tablename__ = 'items'
@@ -38,8 +45,9 @@ class Experiments(Base):
     title = Column(String)
     short_description = Column(Text)
     description = Column(Text)
-    item_list = relationship("Items", secondary=association_table)
+    category_list = relationship("Categories", secondary=association_category_experiment_table)
+    item_list = relationship("Items", secondary=association_item_experiment_table)
     youtube_link = Column(String)
 
     def __repr__(self):
-        return f"Experiments<id:{self.id}, title:{self.title}, short_description:{self.short_description}, description:{self.description}, youtube_link:{self.youtube_link}, item_list:{[item.name for item in self.item_list]}>"
+        return f"Experiments<id:{self.id}, title:{self.title}, short_description:{self.short_description}, description:{self.description}, category_list:{[category.name for category in self.category_list]}, item_list:{[item.name for item in self.item_list]}, youtube_link:{self.youtube_link}>"
