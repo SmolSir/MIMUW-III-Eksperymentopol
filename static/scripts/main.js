36b5b1a2c4d69f7521fa590599ec3218f1b7d700
SmolSir <b.smolarczyk@student.uw.edu.pl>
2024-01-03 20:54:08 +0100

    woooosh

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,8 +4,9 @@
 
 const LOGGER = true;
 
-const FILTERS_ENDPOINT = "/api/available_filters";
-const EXPERIMENTS_ENDPOINT = "";
+const ENDPOINT_PREFIX = "/api";
+const FILTERS_ENDPOINT = ENDPOINT_PREFIX + "/available_filters";
+const EXPERIMENTS_ENDPOINT = ENDPOINT_PREFIX + "/search";
 
 function print(content) {
     if (LOGGER) {
@@ -17,33 +18,135 @@ function print(content) {
 /////////////////////////
 //   FETCH FUNCTIONS   //
 /////////////////////////
+
 async function fetchAvailableFilters() {
-    var filters;
-    var request = await fetch(
-        FILTERS_ENDPOINT,
-        {
-            method: "GET"
+    return fetch(FILTERS_ENDPOINT, { method: "GET" })
+        .then(response => response.json())
+        .catch(error => Promise.reject(error));
+}
+
+async function fetchAvailableExperiments(categoryIdList, itemIdList) {
+
+    function extractCategoryId(string) {
+        return "category" + "=" + string.match(/\d+$/)[0];
+    }
+
+    function extractItemId(string) {
+        return "item" + "=" + string.match(/\d+$/)[0];
+    }
+
+    const querySeparator = "&";
+    const queryEntry = "?";
+
+    var categoriesQuery = categoryIdList.map(extractCategoryId).join(querySeparator);
+    var itemsQuery = itemIdList.map(extractItemId).join(querySeparator);
+
+    const query = EXPERIMENTS_ENDPOINT + queryEntry + categoriesQuery + querySeparator + itemsQuery;
+
+    return fetch(query, { method: "GET" })
+        .then(response => response.json())
+        .catch(error => Promise.reject(error));
+}
+
+
+///////////////////////////////
+//   INTERACTIVE FUNCTIONS   //
+///////////////////////////////
+
+window.onload = function init() {
+
+    function compareByNth(A, B, n) {
+        return A[n - 1].localeCompare(B[n - 1]);
+    }
+
+    function compareByField(A, B, field) {
+        return A[field].localeCompare(B[field]);
+    }
+
+    fetchAvailableFilters()
+        .then(({ categories, items }) => {
+            categories.sort((categoryA, categoryB) => compareByNth(categoryA, categoryB, 2));
+            items.sort((itemA, itemB) => compareByNth(itemA, itemB, 2));
+
+            buildCategoryList(categories);
+            buildSuggestionList(items);
+            buildItemList(items);
+        })
+        .catch(error => {
+            console.error("Failed to fetch data: ", error);
+        });
+
+    const categoryIdList = Array
+        .from(document.querySelectorAll('#category_list input[type="checkbox"]'))
+        .map(category => category.id);
+
+    const itemIdList = Array
+        .from(document.querySelectorAll('#item_list_not_checked input[type="checkbox"]'))
+        .map(item => item.id);
+
+    fetchAvailableExperiments(categoryIdList, itemIdList)
+        .then(experimentsJson => {
+            var experiments = experimentsJson.experiment_list;
+
+            experiments.sort(
+                (experimentA, experimentB) => compareByField(experimentA, experimentB, "title")
+            );
+
+            buildExperimentList(experiments);
+        })
+        .catch(error => {
+            console.error("Failed to fetch data: ", error);
+        });
+}
+
+function markItemChecked(item) {
+
+    function markItemCheckbox(itemCheckbox) {
+        var checkbox = itemCheckbox.querySelector('.form-check-input');
+        var label = itemCheckbox.querySelector('.form-check-label');
+
+        if (label.textContent.trim() === item) {
+            checkbox.checked = true;
+            return true;
         }
-    );
 
-    filters = await request.json();
+        return false;
+    }
 
-    print(filters);
+    var itemListChecked = document.querySelectorAll('#item_list_checked .form-check');
+    var itemListNotChecked = document.querySelectorAll('#item_list_not_checked .form-check');
+    var itemFound = false;
 
-    return filters.error ? -1 : filters;
+    itemListChecked.forEach(function(itemCheckbox) {
+        itemFound |= markItemCheckbox(itemCheckbox);
+    });
+
+    itemListNotChecked.forEach(function(itemCheckbox) {
+        itemFound |= markItemCheckbox(itemCheckbox);
+    });
+
+    return itemFound;
 }
 
+var itemSearchInput = document.getElementById("item_search");
 
-//////////////////////////////
-//   COMPARISON FUNCTIONS   //
-//////////////////////////////
+itemSearchInput.addEventListener('input', function(event) {
+    if (event.inputType === 'insertText' && event.data === '\n') {
+        if (markItemChecked(itemSearchInput.value.trim())) {
+            // Successfully marked item as checked
+            itemSearchInput.value = "";
+        }
+        // Failed to mark item as checked
+    }
+})
 
-function compareItems(itemA, itemB) {
-    var itemLabelA = itemA.querySelector('label').textContent;
-    var itemLabelB = itemB.querySelector('label').textContent;
-
-    return itemLabelA.localeCompare(itemLabelB);
-}
+itemSearchInput.addEventListener('change', function(event) {
+    if (markItemChecked(itemSearchInput.value.trim())) {
+        // Successfully marked item as checked
+        itemSearchInput.value = "";
+    }
+    // Failed to mark item as checked
+})
 
 
 ///////////////////////////////
@@ -76,6 +179,14 @@ function buildCategoryLabel(id, name, state) {
     return categoryLabel;
 }
 
+function buildSuggestion(id, name, state) {
+    const itemOption = document.createElement("option");
+
+    itemOption.value = name.toString();
+
+    return itemOption;
+}
+
 function buildItem(id, name, state) {
     const item = document.createElement("div");
     const itemInput = document.createElement("input");
@@ -95,6 +206,63 @@ function buildItem(id, name, state) {
     item.appendChild(itemLabel);
 
     return item;
+}
+
+function buildExperimentImagePart(imagePath) {
+    const experimentImageContainer = document.createElement("div");
+    const experimentImage = document.createElement("img");
+
+    experimentImage.classList.add("rounded");
+    experimentImage.classList.add("img-fluid");
+    experimentImage.classList.add("mb-2");
+    experimentImage.classList.add("mb-lg-0");
+    experimentImage.src = imagePath;
+
+    experimentImageContainer.classList.add("col-lg-4");
+    experimentImageContainer.classList.add("col-md-5");
+    experimentImageContainer.classList.add("col-md");
+    experimentImageContainer.appendChild(experimentImage);
+
+    return experimentImageContainer;
+}
+
+function buildExperimentTextPart(id, shortDescription, title) {
+    const experimentTextContainer = document.createElement("div");
+    const experimentTextHeader = document.createElement("h3");
+    const experimentVideoLink = document.createElement("a");
+    const experimentDescription = document.createElement("p");
+
+    const experimentVideoLinkPrefix = "./experiment?id=";
+
+    experimentDescription.textContent = shortDescription;
+
+    experimentVideoLink.classList.add("stretched-link");
+    experimentVideoLink.href = experimentVideoLinkPrefix + id.toString();
+    experimentVideoLink.textContent = title;
+
+    experimentTextHeader.appendChild(experimentVideoLink);
+
+    experimentTextContainer.classList.add("col");
+    experimentTextContainer.appendChild(experimentTextHeader);
+    experimentTextContainer.appendChild(experimentDescription);
+
+    return experimentTextContainer;
+}
+
+function buildExperiment(id, imagePath, shortDescription, title) {
+    const experiment = document.createElement("div");
+    const experimentContainer = document.createElement("div");
+
+    experimentContainer.classList.add("row");
+    experimentContainer.classList.add("position-relative");
+    experimentContainer.appendChild(buildExperimentImagePart(imagePath));
+    experimentContainer.appendChild(buildExperimentTextPart(id, shortDescription, title));
+
+    experiment.classList.add("experiments-list-item");
+    experiment.classList.add("mb-3");
+    experiment.appendChild(experimentContainer);
+
+    return experiment;
 }
 
 
@@ -117,7 +285,20 @@ function buildCategoryList(categoryRecords) {
     categoryList.replaceChildren(...categoryChildrenList);
 }
 
+function buildSuggestionList(itemRecords) {
+    const suggestionList = document.getElementById("item_search_suggestion_list");
+    var suggestionChildrenList = [];
+
+    for (const [id, name] of itemRecords) {
+        const suggestion = buildSuggestion(id, name, false);
+        suggestionChildrenList.push(suggestion);
+    }
+
+    suggestionList.replaceChildren(...suggestionChildrenList);
+}
+
 function buildItemList(itemRecords) {
+    document.getElementById("item_list_checked").innerHTML = "";
     const itemList = document.getElementById("item_list_not_checked");
     var itemChildrenList = [];
 
@@ -127,6 +308,18 @@ function buildItemList(itemRecords) {
     }
 
     itemList.replaceChildren(...itemChildrenList);
+}
+
+function buildExperimentList(experimentRecords) {
+    const experimentList = document.getElementById("experiment_list");
+    var experimentChildrenList = [];
+
+    for (const {id, image_path, short_description, title} of experimentRecords) {
+        const experiment = buildExperiment(id, image_path, short_description, title);
+        experimentChildrenList.push(experiment);
+    }
+
+    experimentList.append(...experimentChildrenList);
 }
 
 
@@ -148,6 +341,13 @@ function updateItemList() {
     function itemNotChecked(node) {
         var checkbox = node.querySelector('input[type="checkbox"]');
         return checkbox && !checkbox.checked;
+    }
+
+    function compareItems(itemA, itemB) {
+        var itemLabelA = itemA.querySelector('label').textContent;
+        var itemLabelB = itemB.querySelector('label').textContent;
+
+        return itemLabelA.localeCompare(itemLabelB);
     }
 
     const itemListChecked = document.getElementById("item_list_checked");
